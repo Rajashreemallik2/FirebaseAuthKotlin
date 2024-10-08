62330e9fa84b4b9355b17a7ee58f3020091784d4
Rajashreemallik2 <rajashreemallik2@gmail.com>
2024-10-08 14:07:24 +0530

    added custom login activity with ui screen and intent to switch.

--- app/src/main/java/com/example/firebaseauthkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauthkotlin/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.firebaseauthkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the root view of the activity
+        val button = findViewById<Button>(R.id.helloButton)
+
+        // Set an OnClickListener on the root view
+        button.setOnClickListener {
+            // Create an Intent to start SecondActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            // Start SecondActivity
+            startActivity(intent)
+        }
     }
 }
